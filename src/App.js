bcc41abaf70b38d990504221901be3af4a15a72b
olaunder <olaunder@gmail.com>
2023-06-28 01:27:49 +1000

    PokeGimps
    Project for our upcoming Pokemon Channel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import logo from './logo2.png';
+import latestvideos from './latestvideos.png';
+import './App.scss'
+import YouTubeEmbed from './YouTubeEmbed';
+import propTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const baseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'
+    const [videos, setVideos] = useState([]);
+    const [currentChannelId, setCurrentChannelId] = useState('UCBHD6Yg8R1yS9akfGm4mecQ');
+    const [currentVideo, setCurrentVideo] = useState([]);
 
-export default App;
+    useEffect(() => {
+        (async () => {
+            const data = await fetch(`${baseUrl}${currentChannelId}`)
+            .then(response => response.json());
+
+            setVideos(data.items);
+            console.log(data.items);            
+            setCurrentVideo(data.items[0].link.slice(-11));
+        })();
+    }, [currentChannelId]);
+
+ //   console.log(currentVideo);
+
+    
+    return (
+        <div className="app-container">
+            <div>
+                <img src={logo} alt="Pokemans4Lyf" height="110" width="450"></img>
+            </div>
+            <div>
+                <img src={latestvideos} alt="Latest Videos!" height="100" width="300"></img>
+                </div>
+            <div className='video-responsive'>
+                <iframe
+                    width="853"
+                    height="480"
+                    src={`https://www.youtube.com/embed/${currentVideo}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                />
+            </div>
+            <div className="videos">
+                {videos.map(video => (
+                    <div key ={video.guid} className="videos__item">
+                        <div className="video__image">
+                            <a target="_blank" href={video.link}>
+                                <img src={`https://i4.ytimg.com/vi/${video.guid.split(':')[2]}/mqdefault.jpg`} //onClick={setCurrentVideo(video.link.slice(-11))}
+                                />
+                            </a>
+                        </div>
+                        <div className="video__footer">
+                            <p>{video.title}</p>
+                        </div>
+                    </div>
+                ))
+                }
+            </div>
+        </div>
+)};
---
+++ src/YouTubeEmbed.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import propTypes from 'prop-types'
+
+const YouTubeEmbed = ({currentVideo}) => (
+    <div className='video-responsive'>
+        <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/${currentVideo}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        />
+
+    </div>
+);
+
+YouTubeEmbed.propTypes = {
+    currentVideo:propTypes.string.isRequired
+}
+
+export default YouTubeEmbed
